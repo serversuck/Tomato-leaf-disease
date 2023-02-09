5fd6c869ba040cd8b5d1998f4eb5f83a45c22c70
sorapong <33795437+serversuck@users.noreply.github.com>
2023-02-09 09:39:50 +0700

    Update leaf_stram.py

--- leaf_stram.py
+++ leaf_stram.py
@@ -7,7 +7,8 @@ import numpy as np
 import PIL
 import matplotlib.pyplot as plt
 
-model=load_model("tomato_disease.h5")
+#model=load_model("tomato_disease.h5")
+model=load_model("model.h5")
 
 st.set_page_config(layout='wide')
 
@@ -15,9 +16,11 @@ st.markdown("<h1 style ='text-align: center;color: red'>**ทำนายโร�
 
 img=st.file_uploader("Please upload the image in JPG or PNG format",type=['jpg'])
 
-label={0: 'Bacterial spot',1: ' Early blight',2: ' Late blight',3: 'Leaf Mold',4: ' Septoria leaf_spot',\
-    5: ' Spider mites Two-spotted_spider_mite',6: 'Target Spot',7: 'Tomato Yellow_Leaf_Curl_Virus',8: 'Tomato mosaic_virus',\
-        9: 'healthy'}
+# label={0: 'Bacterial spot',1: ' Early blight',2: ' Late blight',3: 'Leaf Mold',4: ' Septoria leaf_spot',\
+#     5: ' Spider mites Two-spotted_spider_mite',6: 'Target Spot',7: 'Tomato Yellow_Leaf_Curl_Virus',8: 'Tomato mosaic_virus',\
+#         9: 'healthy'}
+
+label={0: 'ยุงลาย',1: 'ยุงรำคาญ'}
 st.text(label)
 
 if img is not None:
@@ -33,17 +36,17 @@ if img is not None:
         )
 
     image=PIL.Image.open(img)
-    newsize = (256, 256)
+    newsize = (128, 128)
     image = image.resize((newsize))
     img_arr = np.array(image)
     scaled_img=img_arr/255
     st.image(image)
     st.text(f"Uploaded Image size is {img_arr.shape}")
     def image_processing(image_array):
-        return image_array.reshape((1,)+(256,256,3))
+        return image_array.reshape((1,)+(128,128,3))
     
-    if img_arr.shape[0:2] != (256,256):
-        re_sized_img=image.resize((256,256))
+    if img_arr.shape[0:2] != (128,128):
+        re_sized_img=image.resize((128,128))
         re_sized_arr=np.array(re_sized_img)
         # re_sized_arr=cv2.resize(scaled_img, (256,256),interpolation=cv2.INTER_CUBIC)
         st.text(f"Uploaded Image is re-sized to {re_sized_arr.shape}")
